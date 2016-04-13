4e2376697606df95e136c85b877e5451f03fc9cc
Jiri Dorazil <juradee@gmail.com>
2016-04-13 17:14:37 +0200

    chybove hlasky

--- lang/cs/lang.php
+++ lang/cs/lang.php
@@ -2,4 +2,6 @@
 
 $lang['enter_skautis'] = "Přihlásit přes SkautIs";
 $lang['badlogin'] = 'Přihlášení se nezdařilo';
+$lang['nouser'] = 'Přihlášení se nezdařilo. Uživatel neexistuje';
+$lang['badskautis'] = 'Chyba ověření SkautIsem';
 ?>
--- lang/en/lang.php
+++ lang/en/lang.php
@@ -2,4 +2,6 @@
 
 $lang['enter_skautis'] = "Sign in with SkautIs";
 $lang['badlogin'] = 'Sorry, login incorrect.';
+$lang['nouser'] = 'Sorry, username not registered.';
+$lang['badskautis'] = 'Sorry, skautis authentication error.';
 ?>
