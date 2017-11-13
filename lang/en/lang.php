406ad92a562a9ff9c46f3cde08f189505b270a54
Michal Janata <mjanata@gmail.com>
2017-11-13 11:02:42 +0100

    Oprava slova skautIS

--- lang/en/lang.php
+++ lang/en/lang.php
@@ -1,7 +1,7 @@
 <?php
 
-$lang['enter_skautis'] = "Sign in with SkautIs";
+$lang['enter_skautis'] = "Sign in with skautIS";
 $lang['badlogin'] = 'Sorry, login incorrect.';
 $lang['nouser'] = 'Sorry, username not registered.';
-$lang['badskautis'] = 'Sorry, skautis authentication error.';
+$lang['badskautis'] = 'Sorry, skautIS authentication error.';
 ?>
